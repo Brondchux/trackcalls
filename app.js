163984cdb0aed7fe1663906c2628d48743ea0e9d
Gospel Chukwu <brondchux@gmail.com>
2022-08-04 22:14:43 -0400

    Ensured the manual and dynamic fields is only displayed one at a time

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-import demoKey from "./demoKey.js";
 import functions from "./utils/functions.js";
 import constants from "./utils/constants.js";
-import { displayCurrentYear } from "./utils/helpers.js";
+import {
+	displayCurrentYear,
+	displayResponse,
+	urlBuilder,
+} from "./utils/helpers.js";
 
 const functionsEl = document.querySelector("#functions");
-const codeEl = document.querySelector("code");
 const dynamicEl = document.querySelector("#dynamic");
+const manualEl = document.querySelector("#manual");
 const sendBtn = document.querySelector("#send-btn");
 
-// auto extract all functions into options;
+// on load, auto extract all functions into options;
 (() => {
 	if (!functions || !functionsEl) return;
 
@@ -22,33 +25,32 @@ const sendBtn = document.querySelector("#send-btn");
 })();
 
 const generateFields = () => {
+	dynamicEl.innerHTML = "";
+	manualEl.classList.add("hide");
 	const functionsValue = functionsEl.value;
-	if (functionsValue === constants.SELECT) return;
+	if (functionsValue === constants.SELECT)
+		return sendBtn.parentElement.classList.add("hide");
+
 	// display button
 	sendBtn.parentElement.classList.remove("hide");
 
 	// process manual query
-	if (functionsValue === constants.OTHERS) {
-		const manualEl = document.querySelector("#manual");
+	if (functionsValue === constants.OTHERS)
 		return manualEl.classList.remove("hide");
-	}
 
 	// process dynamic query
 	const { required } = functions[functionsValue];
-	if (!required && !required.lengtn) return;
-	dynamicEl.innerHTML = "";
+	if (!required || !required.lengtn) return;
 
 	// create the required fields
 	required.forEach((field) => {
 		const divEl = document.createElement("div");
 		const labelEl = document.createElement("label");
 		const selectEl = document.createElement("select");
-
 		labelEl.setAttribute("for", field.param);
 		labelEl.textContent = `Select ${field.param} type:`;
 		selectEl.setAttribute("class", "form-control");
 		selectEl.setAttribute("id", field.param);
-
 		// loop to create & insert options
 		field.values.map((value) => {
 			const option = document.createElement("option");
@@ -56,7 +58,6 @@ const generateFields = () => {
 			option.innerText = value;
 			selectEl.append(option);
 		});
-
 		divEl.append(labelEl);
 		divEl.append(selectEl);
 		dynamicEl.append(divEl);
@@ -66,6 +67,7 @@ const generateFields = () => {
 const sendRequest = async (e) => {
 	e.preventDefault();
 	const functionsValue = functionsEl.value;
+	if (functionsValue === constants.SELECT) return;
 	let queryString = ``;
 
 	// manual query
@@ -87,32 +89,6 @@ const sendRequest = async (e) => {
 	}
 	const res = await urlBuilder(queryString);
 	return displayResponse(res);
-};
-
-const urlBuilder = async (queryString) => {
-	const url = `${constants.BASEURL}/query?${queryString}&apikey=${demoKey}`;
-	const maskedUrl = url.replace(demoKey, constants.DEMO_KEY_MASK);
-	const response = await apiCall(url);
-	return { url: maskedUrl, response };
-};
-
-const apiCall = async (url) => {
-	return await fetch(url, {
-		method: "GET",
-	})
-		.then((res) => res.json())
-		.then((data) => data)
-		.catch((e) => new Error("You got this error: ", e));
-};
-
-const displayResponse = async (result) => {
-	codeEl.innerHTML = "";
-	const preEl = document.createElement("pre");
-	const pEl = document.createElement("p");
-	pEl.textContent = `URL: ${result.url}`;
-	preEl.textContent = `${JSON.stringify(result.response, null, 3)}`;
-	codeEl.append(pEl);
-	codeEl.append(preEl);
 };
 
 sendBtn.addEventListener("click", sendRequest);
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,35 @@
+import constants from "./constants.js";
+import demoKey from "../demoKey.js";
+
 export const displayCurrentYear = () => {
 	const currentYearEl = document.querySelector("#currentYear");
 	const year = new Date().getFullYear();
 	currentYearEl.textContent = year;
+};
+
+export const displayResponse = async (result) => {
+	const codeEl = document.querySelector("code");
+	codeEl.innerHTML = "";
+	const preEl = document.createElement("pre");
+	const pEl = document.createElement("p");
+	pEl.textContent = `URL: ${result.url}`;
+	preEl.textContent = `${JSON.stringify(result.response, null, 3)}`;
+	codeEl.append(pEl);
+	codeEl.append(preEl);
+};
+
+export const apiCall = async (url) => {
+	return await fetch(url, {
+		method: "GET",
+	})
+		.then((res) => res.json())
+		.then((data) => data)
+		.catch((e) => new Error("Ding! You got an error: ", e));
+};
+
+export const urlBuilder = async (queryString) => {
+	const url = `${constants.BASEURL}/query?${queryString}&apikey=${demoKey}`;
+	const maskedUrl = url.replace(demoKey, constants.DEMO_KEY_MASK);
+	const response = await apiCall(url);
+	return { url: maskedUrl, response };
 };
