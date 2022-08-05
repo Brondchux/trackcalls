14e03320f2a6388424e4ee012106634fe7f79b7e
Gospel Chukwu <brondchux@gmail.com>
2022-08-05 16:33:21 -0400

    Unignored demokey as vanilla js had issues accessing .env files

---
+++ js/demoKey.js
@@ -0,0 +1,6 @@
+/**
+ * Demo key, valid for developement only
+ * Will be replaced with a .env value before production
+ */
+
+export default "P3RHY4WRL617B1A5";
--- js/utils/helpers.js
+++ js/utils/helpers.js
@@ -1,6 +1,5 @@
 import constants from "./constants.js";
-import demoApiKey from "../demoKey.js";
-const demoKey = demoApiKey || process.env.DEMO_API_KEY;
+import demoKey from "../demoKey.js";
 
 export const displayCurrentYear = () => {
 	const currentYearEl = document.querySelector("#currentYear");
